784795c16d633e976a5c964985b3824dce4beee2
Binatik <sadxas999@mail.ru>
2022-05-10 23:34:33 +0300

    протокол https  у pen

--- src/pen/API/postService.js
+++ src/pen/API/postService.js
@@ -1,6 +1,6 @@
 export class postService {
   static async getPosts(limit) {
-    const response = await fetch(`http://jsonplaceholder.typicode.com/posts/?_limit=${limit}`);
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/?_limit=${limit}`);
     return response.json();
   }
 }
