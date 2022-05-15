87a07c6b50f9796fa30d4b6865755ebb717fa55a
Binatik <sadxas999@mail.ru>
2022-05-15 21:07:41 +0300

    фиксы touch

--- src/components/postList/Item.jsx
+++ src/components/postList/Item.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
 import { useDebounce } from "@src/hooks/useDebounce";
@@ -9,19 +9,20 @@ import { Article } from "./Article";
 
 const Item = ({ ...props }) => {
   const [cordinate, setCordinate] = useState({
-    cordinateX: null,
-    cordinateY: null,
-    cordinateTouch: 0,
+    x: null,
+    y: null,
+    startX: null,
+    elementX: null,
     direction: null,
   });
 
   function postTouchStart(event) {
     const touch = event.touches[0];
+    const cordinateStartX = touch.pageX - event.target.getBoundingClientRect().x;
 
     const clientX = touch.clientX;
     const clientY = touch.clientY;
-
-    setCordinate({ ...cordinate, cordinateX: clientX, cordinateY: clientY });
+    setCordinate({ ...cordinate, x: clientX, y: clientY, startX: cordinateStartX });
   }
 
   function postTouchMove(event) {
@@ -29,26 +30,27 @@ const Item = ({ ...props }) => {
 
     const clientX = touch.clientX;
     const clientY = touch.clientY;
-    const cordinateTouchX = touch.pageX;
+    const cordinateElement = touch.pageX - cordinate.startX;
 
     const difference = {
-      differenceX: clientX - cordinate.cordinateX,
-      differenceY: clientY - cordinate.cordinateY,
+      differenceX: clientX - cordinate.x,
+      differenceY: clientY - cordinate.y,
     };
 
     const { differenceX, differenceY } = difference;
 
     if (Math.abs(differenceX) > Math.abs(differenceY)) {
-      const text = differenceX > 0 ? "right" : "left";
+      const textDirection = differenceX > 0 ? "right" : "left";
 
-      setCordinate({ ...cordinate, cordinateTouch: cordinateTouchX, direction: text });
+      setCordinate({ ...cordinate, elementX: cordinateElement, direction: textDirection });
     }
   }
-  const touchMove = useDebounce(postTouchMove, 3);
+
+  const touchMove = useDebounce(postTouchMove, 7);
 
   return (
     <>
-      <Item.Item style={{transform: `translateX(${cordinate.cordinateTouch}px)`}} cordinate={cordinate} onTouchMove={touchMove} onTouchStart={postTouchStart}>
+      <Item.Item style={{ transform: `translateX(${cordinate.elementX}px)` }} onTouchMove={touchMove} onTouchStart={postTouchStart}>
         <Avatar />
         <Article {...props} />
       </Item.Item>
@@ -65,6 +67,7 @@ Item.Item = styled.div`
   padding: 15px;
   background-color: ${props => props.theme.colors.surface};
   border-radius: 7px;
+  transition: 0.1s;
 `;
 
 export { Item };
